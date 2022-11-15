360f5672c5e9a86f1c26f2b5efccd80ddd5be16e
Karen Liu <karenq.liu@mail.utoronto.ca>
2022-11-15 14:07:40 -0500

    Fixed small typos.

--- R/visualize_method_comparison.R
+++ R/visualize_method_comparison.R
@@ -30,6 +30,7 @@ visualize_performances_barchart <- function(daa_output) {
   graph_input <- data.frame(methods, significance, value)
 
 
+  # Create stacked barchart
   pl <- ggplot2::ggplot(graph_input, aes(fill=significance, y=value, x=methods)) +
     ggplot2::geom_bar(position="fill", stat="identity") +
     ggplot2::ggtitle("Proportion of Taxa found Significantly Different \nbetween Conditions by Method") +
@@ -42,32 +43,32 @@ visualize_performances_barchart <- function(daa_output) {
 
 #' Visualize overlap between differential abundance analysis (DAA) method results
 #'
-#' `visualize_overlap_pichart()` visualizes the overlap between taxon found significant
+#' `visualize_overlap_piechart()` visualizes the overlap between taxon found significant
 #' identified by three popular current methods of microbial differential abundance analysis,
 #' DESeq2, ANCOM-BC, and ALDEx2.
 #'
 #' @param daa_output a data frame containing the results of the three methods, for format refer
 #' to output from germseq::compare_DAA_methods()
 #'
-#' @returns A pichart containing the proportion of Taxa found significant by different
+#' @returns A pie chart containing the proportion of Taxa found significant by different
 #' numbers of methods.
 #'
 #' @examples
 #' library(germseq)
 #' data("atlas1006_output")
-#' visualize_overlap_pichart(atlas1006_output)
+#' visualize_overlap_piechart(atlas1006_output)
 #'
 #' @export
-visualize_overlap_pichart <- function(daa_output) {
+visualize_overlap_piechart <- function(daa_output) {
 
-  N <- nrow(daa_output)
+  # Modify data.frame for suitable input into ggplot
   score_summ <- table(daa_output$score)
 
   graph_input <- data.frame(method =  c("no methods", "one method",
                                         "two methods", "all methods"),
                             value = c(as.numeric(score_summ)))
 
-
+  # Create pie chart
   pie <- ggplot2::ggplot(graph_input, aes(fill=method, y=value, x="")) +
     ggplot2::geom_bar(width = 1, stat="identity") +
     ggplot2::ggtitle("Summary of Number of Methods finding Taxa Significant") +
