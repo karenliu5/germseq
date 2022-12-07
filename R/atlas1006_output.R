1fe2261b56e67cc318f2ea7faf0517e110a05969
Karen Liu <karenq.liu@mail.utoronto.ca>
2022-12-07 15:47:00 -0500

    Added new atlas1006 output to data

--- R/atlas1006_output.R
+++ R/atlas1006_output.R
@@ -3,12 +3,14 @@
 #' Output from running Atlas1006 data through compare_DAA_methods()
 #'
 #' @format ## `atlas1006_output`
-#' A data frame with 115 rows and 5 columns:
+#' A data frame with 115 rows and 6 columns:
 #' \describe{
 #'   \item{taxon}{Taxon name}
-#'   \item{aldex2, ancombc, deseq2}{Whether each method found a significant DAA}
-#'   \item{score}{Number of methods that found the taxon to be significant}
-#'   ...
+#'   \item{aldex2, ancombc, deseq2}{Benjamini-Hochberg adjusted p-value
+#'   resulting from the respective DAA analysis}
+#'   \item{fisher_p}{Resulting p-value from the Fisher method meta-analysis of the aldex2,
+#'   ancombc, and deseq2 adjusted p-values}
+#'   \item{rawcount}{Number of methods that found the taxon to be significant}
 #' }
 #' @source library(microbiome)
 "atlas1006_output"
