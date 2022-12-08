84a34b6c50f460049103fb380b43add0e4a17768
Karen Liu <karenq.liu@mail.utoronto.ca>
2022-12-08 16:38:57 -0500

    CITATIOn

--- R/plot_result_heatmap.R
+++ R/plot_result_heatmap.R
@@ -23,7 +23,10 @@
 #'
 #' @export
 #' @importFrom magrittr "%>%"
+#'
 plot_result_heatmap <- function(daa_output){
+
+  # Mutate output to get -log p-values
   daa_output <- daa_output %>%
     dplyr::mutate(
       deseq2 = -log10(deseq2),
@@ -32,19 +35,23 @@ plot_result_heatmap <- function(daa_output){
       fisher_p = -log10(fisher_p)
     )
 
+  # Change format of dataframe for input into graph
   heatmap_input <- daa_output %>%
     tidyr::pivot_longer(c(deseq2, ancombc, aldex2, fisher_p),
                         names_to = "method",
                         values_to = "pval")
 
+  # Add additional text for interactive plot
   heatmap_input <- heatmap_input %>%
     dplyr::mutate(text = paste0("Taxon: ", taxon, "\n",
                          "Method: ", method, "\n",
                          "p-value: ", -(pval^10), "\n",
                          "-log10(p): ", round(pval, 4)))
 
+  # Load palette
   pal <- wesanderson::wes_palette("Zissou1", 50, type = "continuous")
 
+  # Create heatmap
   p <- heatmap_input %>%
     ggplot2::ggplot(mapping = aes(x = taxon, y = method, fill = pval, text=text)) +
     ggplot2::geom_tile() +
@@ -55,7 +62,10 @@ plot_result_heatmap <- function(daa_output){
     ggplot2::theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
     ggplot2::ggtitle("Resulting -log10(p) by Method")
 
+  # Make heatmap interactive
   p <- plotly::ggplotly(p, tooltip="text")
 
   return(p)
 }
+
+# [END]
