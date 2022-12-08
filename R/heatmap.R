ccdf9b504486210e1c8d91c0b822dd81f4d96208
Karen Liu <karenq.liu@mail.utoronto.ca>
2022-12-08 03:35:43 -0500

    heatmap update

--- R/heatmap.R
+++ R/heatmap.R
@@ -13,7 +13,22 @@ plot_result_heatmap <- function(daa_output){
   heatmap_input <- daa_output %>%
     tidyr::pivot_longer(c(deseq2, ancombc, aldex2, fisher_p),
                         names_to = "method",
-                        values_to = "-log10(p)")
+                        values_to = "pval")
+
+  heatmap_input <- heatmap_input %>%
+    dplyr::mutate(text = paste0("Taxon: ", taxon, "\n",
+                         "Method: ", method, "\n",
+                         "p-value: ", -(pval^10), "\n",
+                         "-log10(p): ", round(pval, 4)))
 
   pal <- wesanderson::wes_palette("Zissou1", 100, type = "continuous")
+
+  p <- heatmap_input %>% ggplot2::ggplot(aes(x = taxon, y = method, fill = pval)) +
+    geom_tile() +
+    scale_fill_gradientn(colours = pal) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+  p <- plotly::ggplotly(p, tooltip="text")
+
+  return(p)
 }
